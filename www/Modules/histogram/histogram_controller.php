dc9603c6c515af6c9a244a9ea91a696cdbe3990b
Sarrwin <sarrwin10@gmail.com>
2024-12-21 19:46:30 +0800

    Enhancive maintenance activity  for Histogram Controller Module by centralizing repetitive logic into a modular function, significantly improving maintainability and scalability

--- www/Modules/histogram/histogram_controller.php
+++ www/Modules/histogram/histogram_controller.php
@@ -5,149 +5,114 @@ defined('EMONCMS_EXEC') or die('Restricted access');
 
 // Histogram controller
 function histogram_controller() {
-
     global $route, $session, $system_stats;
 
     // HTML view
     if ($route->action == "") {
         $route->format = "html";
-        return view("Modules/histogram/histogram_view.php", array("userid"=>$session['userid']));
+        return view("Modules/histogram/histogram_view.php", array("userid" => $session['userid']));
     }
 
     // System id required
-    $systemid = (int) get('id',true);
+    $systemid = (int)get('id', true);
     // Load system config
-    $config = $system_stats->get_system_config($session['userid'], $systemid);    
+    $config = $system_stats->get_system_config($session['userid'], $systemid);
 
-    // Get kWh vs COP histogram
-    if ($route->action=="kwh_at_cop") {
-        $route->format = "json";
-        // Parameters
-        $params = array(
-            "elec"=>$config->elec,
-            "heat"=>$config->heat,
-            "start"=>get('start',true),
-            "end"=>get('end',true),
-            "div"=>0.1,
-            "interval"=>300,
-            "x_min"=>get('x_min',true),
-            "x_max"=>get('x_max',true)
-        );
-        if ($config->apikey!="") $params['apikey'] = $config->apikey;
-        $result = file_get_contents("$config->server/histogram/data/kwh_at_cop?".http_build_query($params));
-        $output = json_decode($result);
-        if ($output==null) $output = $result;
-        return $output;
-    }
+    // Centralized logic for histogram actions
+    $histogramActions = [
+        "kwh_at_cop" => [
+            "endpoint" => "kwh_at_cop",
+            "params" => [
+                "elec" => $config->elec,
+                "heat" => $config->heat,
+                "div" => 0.1,
+            ]
+        ],
+        "kwh_at_flow" => [
+            "endpoint" => "kwh_at_temperature",
+            "params" => [
+                "power" => $config->heat,
+                "temperature" => $config->flowT,
+                "div" => 0.5,
+            ]
+        ],
+        "kwh_at_outside" => [
+            "endpoint" => "kwh_at_temperature",
+            "params" => [
+                "power" => $config->heat,
+                "temperature" => $config->outsideT,
+                "div" => 0.5,
+            ]
+        ],
+        "kwh_at_flow_minus_outside" => [
+            "endpoint" => "kwh_at_flow_minus_outside",
+            "params" => [
+                "power" => $config->heat,
+                "flow" => $config->flowT,
+                "outside" => $config->outsideT,
+                "div" => 0.5,
+            ]
+        ],
+        "kwh_at_ideal_carnot" => [
+            "endpoint" => "kwh_at_ideal_carnot",
+            "params" => [
+                "power" => $config->heat,
+                "flow" => $config->flowT,
+                "outside" => $config->outsideT,
+                "div" => 0.1,
+            ]
+        ],
+        "flow_temp_curve" => [
+            "endpoint" => "flow_temp_curve",
+            "params" => [
+                "outsideT" => $config->outsideT,
+                "flowT" => $config->flowT,
+                "heat" => $config->heat,
+                "div" => 0.5,
+            ]
+        ]
+    ];
 
-    // Get kWh vs temperature histogram
-    if ($route->action=="kwh_at_flow") {
-        $route->format = "json";
-        // Parameters
-        $params = array(
-            "power"=>$config->heat,
-            "temperature"=>$config->flowT,
-            "start"=>$_GET['start'],
-            "end"=>$_GET['end'],
-            "div"=>0.5,
-            "interval"=>300,
-            "x_min"=>$_GET['x_min'],
-            "x_max"=>$_GET['x_max']
-        );
-        if ($config->apikey!="") $params['apikey'] = $config->apikey;
-        $result = file_get_contents("$config->server/histogram/data/kwh_at_temperature?".http_build_query($params));
-        $output = json_decode($result);
-        if ($output==null) $output = $result;
-        return $output;
-    }
-    
-    // Get kWh vs temperature histogram
-    if ($route->action=="kwh_at_outside") {
-        $route->format = "json";
-        // Parameters
-        $params = array(
-            "power"=>$config->heat,
-            "temperature"=>$config->outsideT,
-            "start"=>$_GET['start'],
-            "end"=>$_GET['end'],
-            "div"=>0.5,
-            "interval"=>300,
-            "x_min"=>$_GET['x_min'],
-            "x_max"=>$_GET['x_max']
-        );
-        if ($config->apikey!="") $params['apikey'] = $config->apikey;
-        $result = file_get_contents("$config->server/histogram/data/kwh_at_temperature?".http_build_query($params));
-        $output = json_decode($result);
-        if ($output==null) $output = $result;
-        return $output;
-    }
-    
-    // Get kWh vs temperature histogram
-    if ($route->action=="kwh_at_flow_minus_outside") {
-        $route->format = "json";
-        // Parameters
-        $params = array(
-            "power"=>$config->heat,
-            "flow"=>$config->flowT,
-            "outside"=>$config->outsideT,
-            "start"=>$_GET['start'],
-            "end"=>$_GET['end'],
-            "div"=>0.5,
-            "interval"=>300,
-            "x_min"=>$_GET['x_min'],
-            "x_max"=>$_GET['x_max']
-        );
-        if ($config->apikey!="") $params['apikey'] = $config->apikey;
-        $result = file_get_contents("$config->server/histogram/data/kwh_at_flow_minus_outside?".http_build_query($params));
-        $output = json_decode($result);
-        if ($output==null) $output = $result;
-        return $output;
-    }
-    
-    // Get kWh vs temperature histogram
-    if ($route->action=="kwh_at_ideal_carnot") {
-        $route->format = "json";
-        // Parameters
-        $params = array(
-            "power"=>$config->heat,
-            "flow"=>$config->flowT,
-            "outside"=>$config->outsideT,
-            "start"=>$_GET['start'],
-            "end"=>$_GET['end'],
-            "div"=>0.1,
-            "interval"=>300,
-            "x_min"=>$_GET['x_min'],
-            "x_max"=>$_GET['x_max']
-        );
-        if ($config->apikey!="") $params['apikey'] = $config->apikey;
-        $result = file_get_contents("$config->server/histogram/data/kwh_at_ideal_carnot?".http_build_query($params));
-        $output = json_decode($result);
-        if ($output==null) $output = $result;
-        return $output;
-    }
-
-    // Get flow temperature curve
-    // Not yet implemented in the histogram view
-    if ($route->action=="flow_temp_curve") {
-        // Parameters
-        $params = array(
-            "outsideT"=>$config->outsideT,
-            "flowT"=>$config->flowT,
-            "heat"=>$config->heat,
-            "start"=>$_GET['start'],
-            "end"=>$_GET['end'],
-            "div"=>0.5,
-            "interval"=>300,
-            "x_min"=>$_GET['x_min'],
-            "x_max"=>$_GET['x_max']
-        );
-        if ($config->apikey!="") $params['apikey'] = $config->apikey;
-        $result = file_get_contents("$config->server/histogram/data/flow_temp_curve?".http_build_query($params));
-        // $output = json_decode($result);
-        // if ($output==null) $output = $result;
-        $route->format = "text";
-        return $result;           
+    if (isset($histogramActions[$route->action])) {
+        return fetch_histogram_data($config, $histogramActions[$route->action]);
     }
 
     return false;
+}
+
+/**
+ * Fetches histogram data based on the configuration and action details.
+ * 
+ * @param object $config The system configuration.
+ * @param array $actionDetails The details of the action to perform.
+ * @return mixed The output from the endpoint or an error message.
+ */
+function fetch_histogram_data($config, $actionDetails) {
+    global $route;
+
+    $route->format = "json";
+
+    // Merge common and specific parameters
+    $params = array_merge($actionDetails['params'], [
+        "start" => get('start', true),
+        "end" => get('end', true),
+        "interval" => 300,
+        "x_min" => get('x_min', true),
+        "x_max" => get('x_max', true)
+    ]);
+
+    // Add API key if available
+    if ($config->apikey != "") {
+        $params['apikey'] = $config->apikey;
+    }
+
+    // Construct the endpoint URL
+    $url = "$config->server/histogram/data/{$actionDetails['endpoint']}?" . http_build_query($params);
+
+    // Fetch the data
+    $result = file_get_contents($url);
+    $output = json_decode($result);
+
+    // Return the result or raw response if JSON decoding fails
+    return $output === null ? $result : $output;
 }
